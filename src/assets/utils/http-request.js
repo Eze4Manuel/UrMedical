2de4a239ed047978f70e155c906221e6eaaec835
J_ode <odeghejerry@yahoo.com>
2021-08-22 21:14:51 +0100

    staging uri

--- src/assets/utils/http-request.js
+++ src/assets/utils/http-request.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 // const uri = 'http://localhost:9005/v1' // local
+
 const uri = 'https://camelogserve.appbuildtest.com/v1' // staging
 // const uri = 'https://api2.appbuiltest.com/api/v1' // live
 const Axios = axios.create({baseURL: uri});
