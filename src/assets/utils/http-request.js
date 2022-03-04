48cbfce691a3a8e224e151ce1f43dc009669fc43
J_ode <odeghejerry@yahoo.com>
2022-03-04 12:33:09 +0100

    setup deployment URI

--- src/assets/utils/http-request.js
+++ src/assets/utils/http-request.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 
-// const uri = 'http://localhost:9005/v1' // local
+const ENV = 'local';
 
-const uri = 'https://camelogserve.appbuildtest.com/v1' // staging
+const getBaseURL = (env) => {
+    if (env === 'local') {
+        return 'http://localhost:9005/v1'
+    }
+
+    if (env === 'staging') {
+        return 'https://camelogserve.appbuildtest.com/v1'
+    }
+
+    return 'https://api.urmed.ng/api/v1'
+}
+
+// const uri = 'http://localhost:9005/v1' // local
+// const uri = 'https://camelogserve.appbuildtest.com/v1' // staging
 // const uri = 'https://api2.appbuiltest.com/api/v1' // live
+
+const uri = getBaseURL(ENV)
 const Axios = axios.create({baseURL: uri});
 export default Axios;
