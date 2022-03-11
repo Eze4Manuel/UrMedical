541d578ea72d0020af23ac409c21a9d7541506d6
eze4manuel <emmanuelezenagu5@gmail.com>
2022-03-11 16:29:31 +0100

    Set baseurl to stagging

--- src/assets/utils/http-request.js
+++ src/assets/utils/http-request.js
@@ -15,9 +15,9 @@ const getBaseURL = (env) => {
 }
 
 // const uri = 'http://localhost:9005/v1' // local
-// const uri = 'https://camelogserve.appbuildtest.com/v1' // staging
+const uri = 'https://camelogserve.appbuildtest.com/v1' // staging
 // const uri = 'https://api2.appbuiltest.com/api/v1' // live
 
-const uri = getBaseURL(ENV)
+// const uri = getBaseURL(ENV)
 const Axios = axios.create({baseURL: uri});
 export default Axios;
